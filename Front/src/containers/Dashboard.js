d73c414761974aa7fa0d24cf6c541f9f2ec8a1c9
Lsael <l.saelens@proton.me>
2023-10-04 15:25:27 +0200

    Resolved a bug on admit dashboard, when user was selecting ticket in different lists (validated, pending etc.), the click event listener was duplicated

--- Front/src/containers/Dashboard.js
+++ Front/src/containers/Dashboard.js
@@ -146,11 +146,11 @@ export default class {
     }
 
     bills.forEach(bill => {
+      $(`#open-bill${bill.id}`).unbind("click")
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
-
   }
 
   getBillsAllUsers = () => {
