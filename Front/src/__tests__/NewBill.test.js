2f84252dc0d037f40e63bcf91df75d9d8821032a
Lsael <l.saelens@proton.me>
2023-10-19 18:08:39 +0200

    Starting to build integration test (POST) for NewBill

--- Front/src/__tests__/NewBill.test.js
+++ Front/src/__tests__/NewBill.test.js
@@ -4,11 +4,15 @@
 
 import { toBeInTheDocument } from '@testing-library/jest-dom';
 import { screen, waitFor } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import NewBillUI from '../views/NewBillUI.js';
 import NewBill from '../containers/NewBill.js';
 import { localStorageMock } from '../__mocks__/localStorage.js';
+import mockStore from '../__mocks__/store.js';
 import { ROUTES, ROUTES_PATH } from '../constants/routes.js';
 import router from '../app/Router.js';
+
+jest.mock('../app/store', () => mockStore);
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
@@ -155,7 +159,7 @@ describe('Given I am connected as an employee', () => {
       expect(submitButton).toHaveAttribute('type', 'submit');
     });
 
-/*     test('When I change the file, it should be updated', async () => {
+    test('When I change the file, it should be updated', async () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
@@ -175,17 +179,128 @@ describe('Given I am connected as an employee', () => {
         localStorage: window.localStorage
       });
 
-      document.body.innerHTML = BillsUI({ data: bills });
+      document.body.innerHTML = NewBillUI();
 
-      const handleClickNewBillFunction = jest.fn(() => BillsDatas.handleClickNewBill());
+      const handleChangeFileFunction = jest.fn(() => newBillsDatas.handleChangeFile);
+      
+      await waitFor(() => screen.getByTestId('file'));
+      const fileButton = screen.getByTestId('file');
+      fileButton.addEventListener('change', handleChangeFileFunction);
+      
+      const fakeFile = new File(['test'], 'test.png', { type: 'image/png' });
+      userEvent.upload(fileButton, fakeFile);
+      
+      expect(handleChangeFileFunction).toHaveBeenCalled();
+    });
 
-      await waitFor(() => screen.getByTestId('btn-new-bill'));
-      const icon = screen.getByTestId('btn-new-bill');
-      icon.addEventListener('click', handleClickNewBillFunction);
+    test('When I change the file, it should be updated', async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
 
-      userEvent.click(icon);
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: 'Employee'
+        })
+      );
 
-      expect(handleClickNewBillFunction).toHaveBeenCalled();
-    }); */
+      const newBillsDatas = new NewBill({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage
+      });
+
+      document.body.innerHTML = NewBillUI();
+
+      const handleSubmitFunction = jest.fn(() => newBillsDatas.handleSubmit);
+      
+      await waitFor(() => screen.getByText('Envoyer'));
+      const submitButton = screen.getByText('Envoyer');
+      submitButton.addEventListener('click', handleSubmitFunction);
+      
+      userEvent.click(submitButton);
+      
+      expect(handleSubmitFunction).toHaveBeenCalled();
+    });
+  });
+});
+
+// test d'intégration POST
+describe('Given I am a user connected as an Employee', () => {
+  describe('When I send my bill, it is added in the bills list', () => {
+    test('fetches bills from mock API GET', async () => {
+      localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+      const root = document.createElement('div');
+      root.setAttribute('id', 'root');
+      document.body.append(root);
+
+      router();
+
+      window.onNavigate(ROUTES_PATH.NewBill);
+
+      await waitFor(() => screen.getByTestId('expense-type'));
+      const expenseSelect = screen.getByTestId('expense-type');
+      const expenseName = screen.getByTestId('expense-name');
+      const datePicker = screen.getByTestId('datepicker');
+      const amount = screen.getByTestId('amount');
+      const tvaAmount = screen.getByTestId('vat');
+      const tvaRate = screen.getByTestId('pct');
+      const commentary = screen.getByTestId('commentary');
+      const filePicker = screen.getByTestId('file');
+
+      expenseSelect.value = "Transports"
+      expenseName.value = "Ma dépense Test"
+      datePicker.value = "01-01-1990"
+      amount.value = 348
+      tvaAmount.value = 70
+      tvaRate.value = 20
+      commentary.value = "Petit voyage quelquepart"
+      filePicker.value = ""
+      
+      // Building POST test
+    });
+    describe('When an error occurs on API', () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, 'bills');
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+        window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+        const root = document.createElement('div');
+        root.setAttribute('id', 'root');
+        document.body.appendChild(root);
+        router();
+      });
+
+      test('fetches bills from an API and fails with 404 message error', async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error('Erreur 404'));
+            }
+          };
+        });
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      });
+
+      test('fetches messages from an API and fails with 500 message error', async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error('Erreur 500'));
+            }
+          };
+        });
+
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/);
+        expect(message).toBeTruthy();
+      });
+    });
   });
 });
